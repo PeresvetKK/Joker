786fa4e5e325257260ef401f93f008c9eb3e9f6e
PeresvetKK <74211480+PeresvetKK@users.noreply.github.com>
2021-09-17 12:34:42 +0300

    Delete joker directory

--- joker/js/script.js
+++
@@ -1,7 +0,0 @@
-function toggle(){
-			var trailer = document.querySelector(".trailer");
-			var video = document.querySelector("video");
-			trailer.classList.toggle("active")
-			video.currentTime = 0;
-			video.pause();
-		}
